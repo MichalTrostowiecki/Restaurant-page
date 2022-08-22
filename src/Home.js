30566e37c513e677cace0ea1f8d1ff723d63c411
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-08-22 13:42:35 +0100

    I've added background styling for main background

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,8 @@ function generateHomePage() {
   const mainDiv = document.createElement("div");
   mainDiv.classList.add("homePage");
 
-  const background = document.createElement("img");
-  background.src = "../assets/mainpage.jpg";
-  background.alt = "background-picture";
-
   mainDiv.appendChild(createTextContent("Welcome to the best restaurant!"));
   mainDiv.appendChild(createTextContent("Browse through our menu"));
-  mainDiv.appendChild(background);
 
   return mainDiv;
 }
